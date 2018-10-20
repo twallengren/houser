82fb915139d771fa6f153e1cd103179ece393725
Toren Wallengren <toren.wallengren@addepar.com>
2018-10-20 13:09:49 -0600

    Wizard state can now update with input boxes

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Wizard extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            "Property Name": "",
+            "Address": "",
+            "City": "",
+            "State": "",
+            "Zip Code": ""
+        }
+
+    }
+
+    inputBoxChange = (event, type) => {
+
+        const input_string = event.target.value;
+
+        this.setState({ [type]: input_string })
+
+    }
+
     render() {
         return (
             <div>
 
-                Wizard
+                <input placeholder="Property Name" onChange={event => this.inputBoxChange(event, "Property Name")} />
+                <input placeholder="Address" onChange={event => this.inputBoxChange(event, "Address")} />
+                <input placeholder="City" onChange={event => this.inputBoxChange(event, "City")} />
+                <input placeholder="State" onChange={event => this.inputBoxChange(event, "State")} />
+                <input placeholder="Zip Code" onChange={event => this.inputBoxChange(event, "Zip Code")} />
 
                 <Link to="/">
                     <div><button>Cancel</button></div>
